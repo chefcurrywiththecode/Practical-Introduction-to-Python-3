bfe12dd08ce3ece2670ff36c2ff7c0d31b8e5d3b
jemberen <nathaniel.jembere@cshs.org>
2022-08-14 11:12:47 -0700

    Script for exercise  4.1

---
+++ Chapter 4/Script for exercise 4.1.py
@@ -0,0 +1,11 @@
+#Script for exercise 4.1 
+
+print('In Ethiopian culture a lot of peole say they are your "cousin"')
+print("It's easy to code once you're on a roll")
+print(''' If you could live anywhere in the world where would it be?
+          If you could go anywhere in the world where would it be?
+          If you could live at any point in history when would it be?
+''')
+print("First thing I like to drink in the morning is coffee\
+       I usually don't eat breakfast in the morning\
+          And my lunch is normally small because I get tired easily")
