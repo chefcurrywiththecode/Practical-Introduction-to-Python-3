3e89793b08dee7fcff332ddb5157e9e8455c907e
jemberen <nathaniel.jembere@cshs.org>
2022-08-14 10:18:29 -0700

    Script for exercise in Chapter 3.1

---
+++ Chapter 3/Script for exercise 3.1.py
@@ -0,0 +1,7 @@
+#First review code of the book 
+print("Liya and Isaac will be coders by the end of this textbook")
+
+is_it_moral="Is it morale to take $500 from you younger cousins?"
+
+print(is_it_moral)
+
