eca353a16a7c89ae10acc91ccb958551a419d0bc
jemberen <nathaniel.jembere@cshs.org>
2022-09-06 09:22:00 -0700

    Exercise 4.2

---
+++ Chapter 4/Script for exercise 4.2.py
@@ -0,0 +1,16 @@
+#Create a string and print it's lenght
+
+string="I am brushing up on my python. "
+
+print(len(string))
+
+string2='They say practice makes perfect'
+
+string3= string + string2
+print(string3)
+
+string4= string + " " + string2
+print(string4)
+
+string5='bazinga'
+print(string5[2:6])
