d03002100b347aae3557d8a5ae4edcd4ac05dd6f
jemberen <nathaniel.jembere@cshs.org>
2022-09-07 09:25:39 -0700

    Script for exercise 4.8

---
+++ Chapter 4/Script for exercise 4.8.py
@@ -0,0 +1,12 @@
+#In one line of code, display the result of trying to .find() the substring "a" in the string "AAA". The result should be -1.
+
+print("AAA".find('a'))
+
+#Replace every occurrence of the character "s" with "x" in the string "Somebody said something to Samantha."
+
+print("Somebody said something to Samantha.".replace("s","x"))
+
+#Write and test a script that accepts user input using the input() function and displays the result of trying to .find() a particular letter in that input.
+
+first_input=input("input your statement")
+print(first_input.find("end"))
