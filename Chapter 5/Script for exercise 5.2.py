d5e0cb2f11e782c0caacb56ef2d451969717e8b0
jemberen <nathaniel.jembere@cshs.org>
2022-09-09 14:44:08 -0700

    Script for exercise 5.2

---
+++ Chapter 5/Script for exercise 5.2.py
@@ -0,0 +1,12 @@
+#Write a script that creates the two variables, num1 and num2
+num1 = 25000000
+num2 = 25_000_000
+
+print (num1, '\n', num2)
+
+#Write a script that assigns the floating-point literal 175000.0 to the variable num using exponential notation, and then prints num in the interactive window.
+num=1.75e5
+print(num)
+
+#try and find the smallest exponent N so that 2e<N>, where <N> is replaced with your number, returns inf.
+print (1e309)
